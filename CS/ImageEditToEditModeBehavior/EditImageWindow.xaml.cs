c25d73fef5da1802075d02321caf3566cad137b2
AndreySlabov <andrey.slabov@devexpress.com>
2023-11-28 13:22:00 +0400

    Upload example

---
+++ CS/ImageEditToEditModeBehavior/EditImageWindow.xaml.cs
@@ -0,0 +1,18 @@
+﻿using DevExpress.Mvvm;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace ImageEditToEditModeBehavior {
+    public partial class EditImageWindow : UserControl {
+        public EditImageWindow() {
+            InitializeComponent();
+        }
+
+        private void editBehavior_CustomEditMenuItems(object sender, DevExpress.Xpf.Editors.CustomEditMenuItemsEventArgs e) {
+            e.EditMenuItems.Clear();
+        }
+    }
+    public class EditImageWindowViewModel : ViewModelBase {
+        public ImageSource Source { get { return GetValue<ImageSource>(); } set { SetValue(value); } }
+    }
+}
---
+++ CS/ImageEditToEditModeBehavior/MainWindow.xaml.cs
@@ -0,0 +1,29 @@
+﻿using DevExpress.Mvvm;
+using DevExpress.Mvvm.DataAnnotations;
+using DevExpress.Xpf.Core;
+using System.Windows.Media;
+
+namespace ImageEditToEditModeBehavior {
+    public partial class MainWindow : ThemedWindow {
+        public MainWindow() {
+            InitializeComponent();
+        }
+    }
+    public class MainViewModel : ViewModelBase {
+        public ImageSource Source { get { return GetValue<ImageSource>(); } set { SetValue(value); } }
+        protected IDialogService DialogService { get { return GetService<IDialogService>(); } }
+
+        [Command]
+        public void Edit() {
+            var imageEditViewModel = new EditImageWindowViewModel() { Source = this.Source };
+            MessageResult result;
+            result = DialogService.ShowDialog(
+                dialogButtons: MessageButton.OKCancel,
+                title: "Edit Image",
+                viewModel: imageEditViewModel
+            );
+            if (result == MessageResult.OK)
+                Source = imageEditViewModel.Source;
+        }
+    }
+}
